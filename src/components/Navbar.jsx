8a465f85a1a34395f7167c801c465251fb38371f
djihane05 <abderrahmanejihen05@gmail.com>
2024-07-31 19:17:03 +0200

    sipmle changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  FaBars,
+  FaTimes,
+ 
+  FaInstagram,
+  FaFacebook,
+  FaLinkedinIn,
+} from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
 import { Link } from 'react-scroll';
-export default function Navbar() {
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className="fixed text-white bg-[#994d00] w-full max-w[1000px] h-[75px] flex justify-between px-4 items-center opacity-75 ">
-    <div>
-      <ul className="flex ">
-        <li>  <Link to='acceuil' smooth={true} duration={500}>
-            Acceuil
-          </Link></li>
-          <li>
-          <Link to='Apropos' smooth={true} duration={500}>
-          A propos ?
-          </Link></li>
-        <li> <Link to='produits' smooth={true} duration={500}>
-            Nos Produits
-          </Link></li>
-        <li> <Link to='contact' smooth={true} duration={500}>
+
+
+      {/* menu */}
+      <ul className='hidden md:flex'>
+        <li>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
             Contact
-          </Link></li>
-       
+          </Link>
+        </li>
       </ul>
-    </div>
-    
-   
-  </div>
-  )
-}
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#994d00] flex flex-col justify-center items-center '
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+     
+     
+      </div>
+  
+  );
+};
+
+export default Navbar;
